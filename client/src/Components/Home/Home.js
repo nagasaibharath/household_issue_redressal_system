011fe840abd55a7c968312fdf1772184c0ebb7cf
SrihariBits <47473205+SrihariBits@users.noreply.github.com>
2019-03-02 18:06:07 +0530

    added fill Issue details page (#3)

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,84 +1,83 @@
-import React, { Component } from 'react';
-import './Home.css'
-import FormLogin from '../Login/FormLogin'
+import React, { Component } from "react";
+import "./Home.css";
+import FormLogin from "../Login/FormLogin";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loginSection: null,
-            users: 1,
-            organizations: 2,
-            freelancers: 5,
-            issues: 7,
-            usersEnd: 574,
-            organizationsEnd: 27,
-            freelancersEnd: 263,
-            issuesEnd: 498,
-        }
-        // this.counter = this.counter.bind(this);
-        // this.count = this.count.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: 1,
+      organizations: 2,
+      freelancers: 3,
+      issues: 4,
+      usersEnd: 574,
+      organizationsEnd: 27,
+      freelancersEnd: 263,
+      issuesEnd: 498
+    };
+    // this.counter = this.counter.bind(this);
+    // this.count = this.count.bind(this);
+  }
 
-    componentDidMount() {
-        this.count();
-        this.setState({ loginSection: (this.props.signinStatus)?null:<FormLogin setView={this.props.setView} setSigninStatus={this.props.setSigninStatus} /> });
-    }
-    
-    counter = () => {
-        if(this.state.users < this.state.usersEnd) {
-            this.setState({users : this.state.users + 4})
-        }
-        if(this.state.organizations < this.state.organizationsEnd) {
-            this.setState({organizations : this.state.organizations + 1})
-        }
-        if(this.state.freelancers < this.state.freelancersEnd) {
-            this.setState({freelancers : this.state.freelancers + 5})
-        }
-        if(this.state.issues < this.state.issuesEnd) {
-            this.setState({issues : this.state.issues + 5})
-        }
-    }
+  componentDidMount() {
+    this.count();
+  }
 
-    count() {
-        setInterval(()=> { this.counter() }, 30);
+  counter = () => {
+    if (this.state.users < this.state.usersEnd) {
+      this.setState({ users: this.state.users + 10 });
     }
-    
-    render() {
-        //let loginSection = (this.props.signinStatus)?null:<FormLogin setView={this.props.setView} setSigninStatus={this.props.setSigninStatus} />;
-        let { users,organizations,freelancers,issues } = this.state;
+    if (this.state.organizations < this.state.organizationsEnd) {
+      this.setState({ organizations: this.state.organizations + 1 });
+    }
+    if (this.state.freelancers < this.state.freelancersEnd) {
+      this.setState({ freelancers: this.state.freelancers + 6 });
+    }
+    if (this.state.issues < this.state.issuesEnd) {
+      this.setState({ issues: this.state.issues + 10 });
+    }
+  };
 
-        return (
-            <div id="home">
-                <div id="headerPanel">
-                    <h1>Household Issue Redressal System</h1>
-                </div>
-                <div id="infoLoginPanel" >
-                    <div id="infoSection" >Generally it is observed in case of Metropolitan cities, where the citizens are not so much acquainted with the service providers in their locality because of the non-permanent nature of their jobs, they find it difficult to contact service providers to address their  problems like improper water supply, bad sanitation and electricity failures.<br />
-                        Welcome to a one-stop portal where the you can fix your issues by contacting freelancing electricians, plumbers, etc to serve your needs.Have a Problem? why wait?Login and lodge a complaint to avail the service. We will monitor the status of the grievance until its fixation.</div>
-                    <span id="loginSection">{this.state.loginSection}</span>
-                </div>
-                <div id="statisicsPanel">
-                    <span id="usersCount">
-                        <h2>{users}</h2>
-                        <h3>Total Users</h3>
-                    </span>
-                    <span id="organizationsCount">
-                        <h2>{organizations}</h2>
-                        <h3>Organizations</h3>
-                    </span>
-                    <span id="freelancersCount">
-                        <h2>{freelancers}</h2>
-                        <h3>FreeLancers</h3>
-                    </span>
-                    <span id="issuesCount">
-                        <h2>{issues}</h2>
-                        <h3>Issues</h3>
-                    </span>
-                </div>
-            </div>
-        );
-    }
+  count() {
+    setInterval(() => {
+      this.counter();
+    }, 60);
+  }
+
+  render() {
+    let loginSection = <FormLogin setView={this.props.setView} />;
+    let { users, organizations, freelancers, issues } = this.state;
+
+    return (
+      <div id="home">
+        <div id="headerPanel">
+          <h1>Household Issue Redressal System</h1>
+        </div>
+        <div id="infoLoginPanel">
+          <div id="infoSection">Hello. Matter goes in this section</div>
+          <span id="loginSection">{loginSection}</span>
+        </div>
+        <div id="statisicsPanel">
+          <span id="usersCount">
+            <h2>{users}</h2>
+            <h3>Total Users</h3>
+          </span>
+          <span id="organizationsCount">
+            <h2>{organizations}</h2>
+            <h3>Organizations</h3>
+          </span>
+          <span id="freelancersCount">
+            <h2>{freelancers}</h2>
+            <h3>FreeLancers</h3>
+          </span>
+          <span id="issuesCount">
+            <h2>{issues}</h2>
+            <h3>Issues</h3>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- client/src/Components/NavBar/TopBar.js
+++ client/src/Components/NavBar/TopBar.js
@@ -1,61 +1,97 @@
-import React, { Component } from 'react';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { Component } from "react";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 class TopBar extends Component {
-
   handleLoginLink = () => {
     this.props.setView("Login");
-  }
+  };
 
   handleLogoutLink = () => {
-    this.props.setSigninStatus(false,"");
-    this.props.setView("Login");
-  }
+    this.props.setView("Home");
+  };
 
   handleRegisterLink = () => {
     this.props.setView("Register");
-  }
+  };
 
   handleHomeLink = () => {
     this.props.setView("Home");
-  }
+  };
+
+  handleDetailsLink = () => {
+    this.props.setView("FillDetails");
+  };
 
   render() {
-    let loginLink,logoutLink,registerLink;
-    if(this.props.signinStatus === true) {
-      logoutLink = <Nav.Link href="#home" onSelect={this.handleLogoutLink} >Logout</Nav.Link>;
-    }
-    else {
-      loginLink = <Nav.Link href="#login" onSelect={this.handleLoginLink} >Login</Nav.Link>;
-      registerLink = <Nav.Link eventKey={2} href="#register" onSelect={this.handleRegisterLink} >Register</Nav.Link>;
+    let loginLink, logoutLink, registerLink;
+    if (this.props.signinStatus == true) {
+      logoutLink = (
+        <Nav.Link href="#home" onSelect={this.handleLogoutLink}>
+          Logout
+        </Nav.Link>
+      );
+    } else {
+      loginLink = (
+        <Nav.Link href="#login" onSelect={this.handleLoginLink}>
+          Login
+        </Nav.Link>
+      );
+      registerLink = (
+        <Nav.Link
+          eventKey={2}
+          href="#register"
+          onSelect={this.handleRegisterLink}
+        >
+          Register
+        </Nav.Link>
+      );
     }
 
-    return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-            <Navbar.Brand href="#home" onClick={this.handleHomeLink} >Issue Redressal</Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="#home" onSelect={this.handleHomeLink} >Home</Nav.Link>
-                  <Nav.Link href="#features">Features</Nav.Link>
-                  <Nav.Link href="#pricing">Pricing</Nav.Link>
-                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Nav>
-                  {registerLink}
-                  {loginLink}
-                  {logoutLink}
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
+    return (
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+      >
+        <Navbar.Brand href="#home" onClick={this.handleHomeLink}>
+          Issue Redressal
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home" onSelect={this.handleHomeLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item
+                href="#action/3.1"
+                onSelect={this.handleDetailsLink}
+              >
+                FillDetails
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            {registerLink}
+            {loginLink}
+            {logoutLink}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
